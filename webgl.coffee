45a290e577869a574ee2877b09ce846b00ec51e8
Michael Hanson <michael.b.hanson@gmail.com>
2012-05-12 14:34:48 -0700

    Stats work

--- webgl.coffee
+++ webgl.coffee
@@ -2,6 +2,7 @@ mesh =
 scene =
 camera =
 renderer =
+stats =
 null
 
 init = ->
@@ -11,20 +12,24 @@ init = ->
   camera.position.z = 1000
   scene.add camera
 
-  geometry = new THREE.CubeGeometry 200, 200, 200
+  geometry = new THREE.CubeGeometry 300, 300, 300
   material = new THREE.MeshBasicMaterial {color: 0xff0000, wireframe: true}
 
   mesh = new THREE.Mesh geometry, material
   scene.add mesh
 
-  renderer = new THREE.WebGLRenderer()
+  renderer = new THREE.WebGLRenderer {antialias: true}
   renderer.setSize window.innerWidth, window.innerHeight
 
   document.body.appendChild renderer.domElement
 
+  setStats()
+
 animate = ->
+  stats.begin()
   requestAnimationFrame animate
   render()
+  stats.end()
 
 render = ->
   mesh.rotation.x += 0.01
@@ -32,5 +37,15 @@ render = ->
 
   renderer.render scene, camera
 
+setStats = ->
+  stats = new Stats()
+  stats.setMode 0
+  stats.domElement.style.position = 'absolute'
+  stats.domElement.style.left = '0px'
+  stats.domElement.style.top = '0px'
+
+  document.body.appendChild stats.domElement
+
 init()
+# setStats()
 animate()
--- webgl.js
+++ webgl.js
@@ -1,7 +1,7 @@
 (function() {
-  var animate, camera, init, mesh, render, renderer, scene;
+  var animate, camera, init, mesh, render, renderer, scene, setStats, stats;
 
-  mesh = scene = camera = renderer = null;
+  mesh = scene = camera = renderer = stats = null;
 
   init = function() {
     var geometry, material;
@@ -9,27 +9,41 @@
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
     camera.position.z = 1000;
     scene.add(camera);
-    geometry = new THREE.CubeGeometry(200, 200, 200);
+    geometry = new THREE.CubeGeometry(300, 300, 300);
     material = new THREE.MeshBasicMaterial({
       color: 0xff0000,
       wireframe: true
     });
     mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
-    renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer({
+      antialias: true
+    });
     renderer.setSize(window.innerWidth, window.innerHeight);
-    return document.body.appendChild(renderer.domElement);
+    document.body.appendChild(renderer.domElement);
+    return setStats();
   };
 
   animate = function() {
+    stats.begin();
     requestAnimationFrame(animate);
-    return render();
+    render();
+    return stats.end();
   };
 
   render = function() {
     mesh.rotation.x += 0.01;
     mesh.rotation.y += 0.02;
     return renderer.render(scene, camera);
+  };
+
+  setStats = function() {
+    stats = new Stats();
+    stats.setMode(0);
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '0px';
+    stats.domElement.style.top = '0px';
+    return document.body.appendChild(stats.domElement);
   };
 
   init();
